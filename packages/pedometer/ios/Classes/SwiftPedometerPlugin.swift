2491ce5013d527da6d3f5651c34bcadbfd34a820
jhlee-danbi <jh.lee@danbicorp.com>
2024-02-14 16:13:28 +0900

    fix: iOS Pedometer 수정

--- packages/pedometer/ios/Classes/SwiftPedometerPlugin.swift
+++ packages/pedometer/ios/Classes/SwiftPedometerPlugin.swift
@@ -73,6 +73,8 @@ public class StepCounter: NSObject, FlutterStreamHandler {
     private let pedometer = CMPedometer()
     private var running = false
     private var eventSink: FlutterEventSink?
+    private var timer: Timer?
+    private var currentDay: Int?
 
     private func handleEvent(count: Int) {
         // If no eventSink to emit events to, do nothing (wait)
@@ -83,6 +85,45 @@ public class StepCounter: NSObject, FlutterStreamHandler {
         eventSink!(count)
     }
 
+
+    private func startDayChangedChecker() {
+        currentDay = Calendar.current.component(.day, from: Date()) // return day
+        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true, block: { [weak self] _ in
+            let newDay = Calendar.current.component(.day, from: Date())
+            if self?.currentDay != newDay {
+                self?.currentDay = newDay
+                self?.restartPedometerUpdates()
+            }
+        })
+    }
+
+    private func restartPedometerUpdates() {
+        if running {
+            pedometer.stopUpdates()
+            DispatchQueue.main.async {
+                self.handleEvent(count: 0)
+            }
+            startPedometerUpdates()
+        }
+    }
+
+    private func startPedometerUpdates() {
+        let nowDate = Date()
+        guard let todayStartDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: nowDate) else {
+            eventSink?(FlutterError(code: "4", message: "Failed to set start date.", details: nil))
+            return
+        }
+        running = true
+        pedometer.startUpdates(from: todayStartDate) {
+            pedometerData, error in
+            guard let pedometerData = pedometerData, error == nil else { return }
+
+            DispatchQueue.main.async {
+                self.handleEvent(count: pedometerData.numberOfSteps.intValue)
+            }
+        }
+    }
+
     public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
         self.eventSink = eventSink
         if #available(iOS 10.0, *) {
@@ -90,18 +131,8 @@ public class StepCounter: NSObject, FlutterStreamHandler {
                 eventSink(FlutterError(code: "3", message: "Step Count is not available", details: nil))
             }
             else if (!running) {
-                let systemUptime = ProcessInfo.processInfo.systemUptime;
-                let timeNow = Date().timeIntervalSince1970
-                let dateOfLastReboot = Date(timeIntervalSince1970: timeNow - systemUptime)
-                running = true
-                pedometer.startUpdates(from: dateOfLastReboot) {
-                    pedometerData, error in
-                    guard let pedometerData = pedometerData, error == nil else { return }
-
-                    DispatchQueue.main.async {
-                        self.handleEvent(count: pedometerData.numberOfSteps.intValue)
-                    }
-                }
+                startPedometerUpdates()
+                startDayChangedChecker()
             }
         } else {
             eventSink(FlutterError(code: "1", message: "Requires iOS 10.0 minimum", details: nil))
@@ -117,6 +148,9 @@ public class StepCounter: NSObject, FlutterStreamHandler {
             pedometer.stopUpdates()
             running = false
         }
+
+        timer?.invalidate() 
+        timer = nil
         return nil
     }
 }
